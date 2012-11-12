ebf68378d72a96d3cfa5402483cda7c167ab6ac6
borja <borya09@gmail.com>
2012-11-12 23:45:51 +0100

    mongodb

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,7 +1,27 @@
-grails {
-    mongo {
-        host = "localhost"
-        port = 27017
-        databaseName = "test"
+
+
+
+environments {
+    development {
+        grails {
+            mongo {
+                host = "localhost"
+                port = 27017
+                databaseName = "test"
+            }
+        }
+
+    }
+    test{
+
+    }
+    production{
+        grails {
+            mongo {
+                host = "localhost"
+                port = 27017
+                databaseName = "test"
+            }
+        }
     }
 }
