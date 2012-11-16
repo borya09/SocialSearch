1e6ee6da7db91fd45130d1bf2cef01a901dc1aab
borja <borya09@gmail.com>
2012-11-16 01:47:16 +0100

    harvest service

--- grails-app/services/com/borja/socialsearch/services/HarvestService.groovy
+++ grails-app/services/com/borja/socialsearch/services/HarvestService.groovy
@@ -1,8 +1,15 @@
 package com.borja.socialsearch.services
 
+import com.borja.socialsearch.apis.IApi
+import com.borja.socialsearch.apis.ApiFactory
+
 class HarvestService {
 
-    def serviceMethod() {
+    def gather(tag) {
+
+        IApi collector = ApiFactory.getInstance(tag)
+
+        def items = collector.searchItems()
 
     }
 }
